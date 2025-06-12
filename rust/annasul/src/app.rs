ddf50857e7412d5e2a50ef319b81ecebf75cc879
air <2283106661@qq.com>
2025-06-12 18:16:35 +0800

    add trait App

--- rust/annasul/src/app.rs
+++ rust/annasul/src/app.rs
@@ -15,5 +15,38 @@
 //! application operation
 //!
 
+use std::borrow::Cow;
+use std::ffi::OsStr;
+use std::fmt::{Display, Formatter, Pointer, Write};
+use std::path::Path;
+
 #[cfg(any(doc, feature = "app-apps"))]
 pub mod apps;
+
+#[derive(Debug)]
+pub enum Error {}
+impl Display for Error {
+    fn fmt(&self, f: &mut Formatter<'_>) -> std::fmt::Result {
+        match self {
+            Error => f.write_str(""),
+        }
+    }
+}
+impl std::error::Error for Error {
+    fn source(&self) -> Option<&(dyn std::error::Error + 'static)> {
+        match self {
+            Error => None,
+        }
+    }
+}
+pub type Result<T> = std::result::Result<T, Error>;
+pub trait App<'a> {
+    fn name() -> Cow<'a, str>;
+    fn version() -> Result<Cow<'a, str>>;
+    fn authors() -> Result<impl Iterator<Item = Cow<'a, str>>>;
+    fn home_page() -> Result<Cow<'a, str>>;
+    fn license() -> Result<Option<Cow<'a, str>>>;
+    fn license_file() -> Result<Option<Cow<'a, Path>>>;
+    fn description() -> Result<Cow<'a, str>>;
+    fn documentation() -> Result<Cow<'a, str>>;
+}
--- rust/annasul/src/os_impl/desktop/app/apps/rust/rustup.rs
+++ rust/annasul/src/os_impl/desktop/app/apps/rust/rustup.rs
@@ -10,4 +10,12 @@
 //
 // You should have received a copy of the GNU Affero General Public License
 // along with this program. If not, see <https://www.gnu.org/licenses/>.
-pub mod rust;
+
+#[derive(Debug)]
+pub struct App {
+    
+}
+
+impl crate::app::App for App {
+
+}
