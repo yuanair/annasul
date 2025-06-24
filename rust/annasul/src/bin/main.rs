40182fdfc7faff809fc8bac619f2b8dcb7839fc2
air <2283106661@qq.com>
2025-06-24 23:51:33 +0800

    [annasul] test gtk4 gui

--- rust/annasul/src/bin/main.rs
+++ rust/annasul/src/bin/main.rs
@@ -10,7 +10,52 @@
 //
 // You should have received a copy of the GNU Affero General Public License
 // along with this program. If not, see <https://www.gnu.org/licenses/>.
-use gtk4::{Application, ApplicationWindow, Button, Entry, Orientation, prelude::*};
+use std::{
+    fmt::{Debug, Display},
+    str::FromStr,
+};
+
+use annasul::app::apps::rust::{HostTriple, Profile, Toolchain};
+use gtk4::{
+    prelude::*,
+    Application,
+    ApplicationWindow,
+    Button,
+    ComboBoxText,
+    Entry,
+    Label,
+    Notebook,
+    Orientation,
+};
+fn build_combo<T: Default + Debug + Display + FromStr + 'static>(
+    combo: ComboBoxText, all: &[T], f: fn(&T),
+) -> ComboBoxText {
+    for i in all.iter() {
+        combo.append(Some(&i.to_string()), &i.to_string());
+    }
+    combo.set_active_id(Some(&T::default().to_string()));
+    combo.connect_changed(move |combo| {
+        let active_id = combo.active_id().and_then(|id| id.parse().ok());
+        if let Some(id) = active_id {
+            let selected = T::from(id);
+            f(&selected);
+        }
+    });
+    combo
+}
+fn build_notebook<T: Debug + Display + FromStr + 'static>(
+    notebook: Notebook, all: &[T], f: fn(i32) -> bool,
+) -> Notebook {
+    for i in all.iter() {
+        notebook.append_page(
+            &Label::new(Some(&i.to_string())),
+            Some(&Label::new(Some(&i.to_string()))),
+        );
+    }
+    notebook.set_current_page(Some(0));
+    notebook.connect_change_current_page(move |notebook, i| f(i));
+    notebook
+}
 #[tokio::main]
 async fn main() -> glib::ExitCode {
     env_logger::init();
@@ -23,15 +68,46 @@ async fn main() -> glib::ExitCode {
             .default_height(600)
             .build();
         window.present();
-        let vbox = gtk4::Box::new(Orientation::Vertical, 10);
-        window.set_child(Some(&vbox));
-        let entry = Entry::builder().placeholder_text("Input").margin_top(20).build();
-        vbox.append(&entry);
-        let button = Button::with_label("Ok");
-        vbox.append(&button);
-        button.connect_clicked(move |_| {
-            println!("Input: {}", entry.text());
-        });
+        let notebook = Notebook::builder().build();
+        window.set_child(Some(&notebook));
+        for i in 1..=3 {
+            let vbox = gtk4::Box::new(Orientation::Vertical, 10);
+            vbox.append(&build_combo(
+                ComboBoxText::new(),
+                &[Profile::Minimal, Profile::Default, Profile::Complete],
+                |profile| {
+                    println!("profile: {profile}");
+                },
+            ));
+            vbox.append(&build_combo(
+                ComboBoxText::new(),
+                &[
+                    Toolchain::Stable,
+                    Toolchain::Beta,
+                    Toolchain::Nightly,
+                    Toolchain::None,
+                ],
+                |toolchain| {
+                    println!("toolchain: {toolchain}");
+                },
+            ));
+            vbox.append(&build_notebook(
+                Notebook::new(),
+                &[HostTriple::Host, HostTriple::Target("".to_string())],
+                |i| {
+                    println!("host: {i}");
+                    true
+                },
+            ));
+            let entry = Entry::builder().placeholder_text("Input").margin_top(20).build();
+            vbox.append(&entry);
+            let button = Button::with_label("Install");
+            vbox.append(&button);
+            button.connect_clicked(move |_| {
+                println!("Input {i}: {}", entry.text());
+            });
+            notebook.append_page(&vbox, Some(&Label::new(Some(&format!("Preset {i}")))));
+        }
     });
     app.run()
 }
--- rust/annasul/src/os_impl/desktop/app/apps/rust.rs
+++ rust/annasul/src/os_impl/desktop/app/apps/rust.rs
@@ -18,6 +18,7 @@ use std::{
     io::Write,
     path::{Path, PathBuf},
     process::{ExitStatus, Stdio},
+    str::FromStr,
 };
 
 use log::{info, trace, warn};
@@ -25,55 +26,55 @@ use serde::{Deserialize, Serialize};
 use tokio::{io::AsyncReadExt, process::Command};
 
 use crate::app::AppLicense;
-#[derive(Default, Debug, Clone, Eq, PartialEq, Hash, Ord, PartialOrd, Serialize, Deserialize,)]
+#[derive(Default, Debug, Clone, Eq, PartialEq, Hash, Ord, PartialOrd, Serialize, Deserialize)]
 pub struct Rustup {
     home_path: PathBuf,
 }
-#[derive(Debug,)]
+#[derive(Debug)]
 pub enum Error {
-    Unsupported(Cow<'static, str,>,),
-    IOError(std::io::Error,),
-    TaskJoinError(tokio::task::JoinError,),
-    InnerError(Cow<'static, str,>,),
+    Unsupported(Cow<'static, str>),
+    IOError(std::io::Error),
+    TaskJoinError(tokio::task::JoinError),
+    InnerError(Cow<'static, str>),
     Failed {
         exit_status: ExitStatus,
-        stdin:       Cow<'static, str,>,
-        stdout:      Cow<'static, str,>,
-        stderr:      Cow<'static, str,>,
+        stdin:       Cow<'static, str>,
+        stdout:      Cow<'static, str>,
+        stderr:      Cow<'static, str>,
     },
     FailedToGetHomeDir,
-    RequestError(reqwest::Error,),
+    RequestError(reqwest::Error),
 }
 impl Display for Error {
-    fn fmt(&self, f: &mut Formatter<'_,>,) -> std::fmt::Result {
+    fn fmt(&self, f: &mut Formatter<'_>) -> std::fmt::Result {
         match self {
-            Error::Unsupported(info,) => f.write_fmt(format_args!("Unsupported: {info}"),),
-            Error::IOError(e,) => f.write_fmt(format_args!("IO error: {e}"),),
-            Error::TaskJoinError(e,) => f.write_fmt(format_args!("Task join error: {e}"),),
-            Error::InnerError(info,) => f.write_fmt(format_args!("Inner error: {info}"),),
-            Error::Failed { exit_status, stdin, stdout, stderr, } => f.write_fmt(format_args!(
+            Error::Unsupported(info) => f.write_fmt(format_args!("Unsupported: {info}")),
+            Error::IOError(e) => f.write_fmt(format_args!("IO error: {e}")),
+            Error::TaskJoinError(e) => f.write_fmt(format_args!("Task join error: {e}")),
+            Error::InnerError(info) => f.write_fmt(format_args!("Inner error: {info}")),
+            Error::Failed { exit_status, stdin, stdout, stderr } => f.write_fmt(format_args!(
                 "Failed:\n - exit status: {exit_status}\n - stdin:\n{stdin}\n\n - \
                  stdout:\n{stdout}\n\n - stderr:\n{stderr}"
-            ),),
-            Error::FailedToGetHomeDir => f.write_fmt(format_args!("failed to get HOME dir"),),
-            Error::RequestError(e,) => f.write_fmt(format_args!("request error: {e}"),),
+            )),
+            Error::FailedToGetHomeDir => f.write_fmt(format_args!("failed to get HOME dir")),
+            Error::RequestError(e) => f.write_fmt(format_args!("request error: {e}")),
         }
     }
 }
 impl std::error::Error for Error {
-    fn source(&self,) -> Option<&(dyn std::error::Error + 'static),> {
+    fn source(&self) -> Option<&(dyn std::error::Error + 'static)> {
         match self {
-            Error::Unsupported(_,) => None,
-            Error::IOError(e,) => Some(e,),
-            Error::TaskJoinError(e,) => Some(e,),
-            Error::InnerError(_,) => None,
+            Error::Unsupported(_) => None,
+            Error::IOError(e) => Some(e),
+            Error::TaskJoinError(e) => Some(e),
+            Error::InnerError(_) => None,
             Error::Failed { .. } => None,
             Error::FailedToGetHomeDir => None,
-            Error::RequestError(e,) => Some(e,),
+            Error::RequestError(e) => Some(e),
         }
     }
 }
-pub type Result<T,> = std::result::Result<T, Error,>;
+pub type Result<T> = std::result::Result<T, Error>;
 #[derive(
     Default, Debug, Copy, Clone, Eq, PartialEq, Hash, Ord, PartialOrd, Serialize, Deserialize,
 )]
@@ -84,12 +85,12 @@ pub enum Toolchain {
     Nightly,
     None,
 }
-#[derive(Default, Debug, Clone, Eq, PartialEq, Hash, Ord, PartialOrd, Serialize, Deserialize,)]
+#[derive(Default, Debug, Clone, Eq, PartialEq, Hash, Ord, PartialOrd, Serialize, Deserialize)]
 pub enum HostTriple {
     #[default]
     Host,
     /// e.g. x86_64-unknown-linux-gnu
-    Target(String,),
+    Target(String),
 }
 #[derive(
     Default, Debug, Copy, Clone, Eq, PartialEq, Hash, Ord, PartialOrd, Serialize, Deserialize,
@@ -100,69 +101,104 @@ pub enum Profile {
     Default,
     Complete,
 }
-#[derive(Debug, Clone, Eq, PartialEq, Hash, Ord, PartialOrd, Serialize, Deserialize,)]
+#[derive(Debug, Clone, Eq, PartialEq, Hash, Ord, PartialOrd, Serialize, Deserialize)]
 pub struct InstallCustomInfo {
     default_host_triple:  HostTriple,
     default_toolchain:    Toolchain,
     profile:              Profile,
     modify_path_variable: bool,
 }
-#[derive(Default, Debug, Clone, Eq, PartialEq, Hash, Ord, PartialOrd, Serialize, Deserialize,)]
+#[derive(Default, Debug, Clone, Eq, PartialEq, Hash, Ord, PartialOrd, Serialize, Deserialize)]
 pub enum InstallInfo {
     #[default]
     Default,
-    Custom(InstallCustomInfo,),
+    Custom(InstallCustomInfo),
 }
 #[cfg(unix)]
-async fn download_rustup_init_sh() -> Result<(),> {
+async fn download_rustup_init_sh() -> Result<()> {
     let url = "https://sh.rustup.rs";
-    let content = reqwest::get(url,)
+    let content = reqwest::get(url)
         .await
-        .map_err(Error::RequestError,)?
+        .map_err(Error::RequestError)?
         .text()
         .await
-        .map_err(Error::RequestError,)?;
-    if !exists("./cache",).map_err(Error::IOError,)? {
-        create_dir("cache",).map_err(Error::IOError,)?;
+        .map_err(Error::RequestError)?;
+    if !exists("./cache").map_err(Error::IOError)? {
+        create_dir("cache").map_err(Error::IOError)?;
     }
-    let mut file = File::create("./cache/rustup-init.sh",).map_err(Error::IOError,)?;
-    file.write_all(content.as_bytes(),).map_err(Error::IOError,)?;
-    Ok((),)
+    let mut file = File::create("./cache/rustup-init.sh").map_err(Error::IOError)?;
+    file.write_all(content.as_bytes()).map_err(Error::IOError)?;
+    Ok(())
 }
 #[cfg(unix)]
-async fn chmod_rustup_init_sh() -> Result<(),> {
+async fn chmod_rustup_init_sh() -> Result<()> {
     use std::os::unix::fs::PermissionsExt;
-    let path = Path::new("./cache/rustup-init.sh",);
-    let metadata = metadata(path,).map_err(Error::IOError,)?;
+    let path = Path::new("./cache/rustup-init.sh");
+    let metadata = metadata(path).map_err(Error::IOError)?;
     let mut permissions = metadata.permissions();
-    permissions.set_mode(permissions.mode() | 0o100,);
-    std::fs::set_permissions(path, permissions,).map_err(Error::IOError,)?;
-    Ok((),)
+    permissions.set_mode(permissions.mode() | 0o100);
+    std::fs::set_permissions(path, permissions).map_err(Error::IOError)?;
+    Ok(())
 }
 impl Display for Toolchain {
-    fn fmt(&self, f: &mut Formatter<'_,>,) -> std::fmt::Result {
+    fn fmt(&self, f: &mut Formatter<'_>) -> std::fmt::Result {
         match self {
-            Toolchain::Stable => f.write_str("stable",),
-            Toolchain::Beta => f.write_str("beta",),
-            Toolchain::Nightly => f.write_str("nightly",),
-            Toolchain::None => f.write_str("none",),
+            Toolchain::Stable => f.write_str("stable"),
+            Toolchain::Beta => f.write_str("beta"),
+            Toolchain::Nightly => f.write_str("nightly"),
+            Toolchain::None => f.write_str("none"),
+        }
+    }
+}
+impl FromStr for Toolchain {
+    type Err = ();
+
+    fn from_str(s: &str) -> std::result::Result<Self, Self::Err> {
+        match s {
+            "stable" => Ok(Toolchain::Stable),
+            "beta" => Ok(Toolchain::Beta),
+            "nightly" => Ok(Toolchain::Nightly),
+            "none" => Ok(Toolchain::None),
+            _ => Err(()),
         }
     }
 }
 impl Display for HostTriple {
-    fn fmt(&self, f: &mut Formatter<'_,>,) -> std::fmt::Result {
+    fn fmt(&self, f: &mut Formatter<'_>) -> std::fmt::Result {
         match self {
-            HostTriple::Host => f.write_str("host",),
-            HostTriple::Target(target,) => f.write_str(target,),
+            HostTriple::Host => f.write_str("host"),
+            HostTriple::Target(target) => f.write_str(target),
+        }
+    }
+}
+impl FromStr for HostTriple {
+    type Err = ();
+
+    fn from_str(s: &str) -> std::result::Result<Self, Self::Err> {
+        match s {
+            "host" => Ok(HostTriple::Host),
+            s => Ok(HostTriple::Target(s.to_string())),
         }
     }
 }
 impl Display for Profile {
-    fn fmt(&self, f: &mut Formatter<'_,>,) -> std::fmt::Result {
+    fn fmt(&self, f: &mut Formatter<'_>) -> std::fmt::Result {
         match self {
-            Profile::Minimal => f.write_str("minimal",),
-            Profile::Default => f.write_str("default",),
-            Profile::Complete => f.write_str("complete",),
+            Profile::Minimal => f.write_str("minimal"),
+            Profile::Default => f.write_str("default"),
+            Profile::Complete => f.write_str("complete"),
+        }
+    }
+}
+impl FromStr for Profile {
+    type Err = ();
+
+    fn from_str(s: &str) -> std::result::Result<Self, Self::Err> {
+        match s {
+            "minimal" => Ok(Profile::Minimal),
+            "default" => Ok(Profile::Default),
+            "complete" => Ok(Profile::Complete),
+            _ => Err(()),
         }
     }
 }
@@ -179,39 +215,37 @@ impl Default for InstallCustomInfo {
 impl crate::app::AppInfo for Rustup {
     type Error = Error;
 
-    async fn name(&self,) -> Cow<'_, str,> { Cow::Borrowed("rustup",) }
+    async fn name(&self) -> Cow<'_, str> { Cow::Borrowed("rustup") }
 
-    async fn license(&self,) -> Result<Cow<'_, AppLicense,>,> {
+    async fn license(&self) -> Result<Cow<'_, AppLicense>> {
         Ok(Cow::Owned(AppLicense::Or(
-            Box::new(AppLicense::Text("Apache".to_string(),),),
-            Box::new(AppLicense::Text("MIT".to_string(),),),
-        ),),)
+            Box::new(AppLicense::Text("Apache".to_string())),
+            Box::new(AppLicense::Text("MIT".to_string())),
+        )))
     }
 
-    async fn description(&self,) -> Result<Cow<'_, str,>,> { todo!() }
+    async fn description(&self) -> Result<Cow<'_, str>> { todo!() }
 
-    async fn documentation(&self,) -> Result<Cow<'_, str,>,> {
-        Ok(Cow::Borrowed("https://rust-lang.github.io/rustup/",),)
+    async fn documentation(&self) -> Result<Cow<'_, str>> {
+        Ok(Cow::Borrowed("https://rust-lang.github.io/rustup/"))
     }
 
-    async fn homepage(&self,) -> Result<Cow<'_, str,>,> { Ok(Cow::Borrowed("https://rustup.rs",),) }
+    async fn homepage(&self) -> Result<Cow<'_, str>> { Ok(Cow::Borrowed("https://rustup.rs")) }
 
-    async fn repository(&self,) -> Result<Cow<'_, str,>,> {
-        Ok(Cow::Borrowed("https://github.com/rust-lang/rustup/",),)
+    async fn repository(&self) -> Result<Cow<'_, str>> {
+        Ok(Cow::Borrowed("https://github.com/rust-lang/rustup/"))
     }
 
-    async fn version(&self,) -> Result<Cow<'_, str,>,> { todo!() }
+    async fn version(&self) -> Result<Cow<'_, str>> { todo!() }
 }
 impl crate::app::AppPath for Rustup {
     type Error = Error;
 
-    async fn home_path(&self,) -> Result<Cow<'_, Path,>,> {
-        Ok(Cow::Borrowed(self.home_path.as_path(),),)
+    async fn home_path(&self) -> Result<Cow<'_, Path>> {
+        Ok(Cow::Borrowed(self.home_path.as_path()))
     }
 
-    async fn bin_path(&self,) -> Result<Cow<'_, Path,>,> {
-        Ok(Cow::Owned(self.home_path.join("bin",),),)
-    }
+    async fn bin_path(&self) -> Result<Cow<'_, Path>> { Ok(Cow::Owned(self.home_path.join("bin"))) }
 }
 impl crate::app::AppOper for Rustup {
     type Error = Error;
@@ -220,22 +254,23 @@ impl crate::app::AppOper for Rustup {
     type RemoveInfo = ();
     type UpdateInfo = ();
 
-    async fn install(info: Self::InstallInfo,) -> Result<Self,> {
-        if cfg!(unix) {
+    async fn install(info: Self::InstallInfo) -> Result<Self> {
+        #[cfg(unix)]
+        {
             use std::os::unix::ffi::OsStringExt;
             trace!("Installing Rustup with info: {info:?}");
             download_rustup_init_sh().await?;
             trace!("Downloaded rustup-init.sh successfully");
             chmod_rustup_init_sh().await?;
             trace!("Chmod rustup-init.sh successfully");
-            let shell: Cow<'static, str,> = match info {
+            let shell: Cow<'static, str> = match info {
                 InstallInfo::Default => "./cache/rustup-init.sh -y".into(),
                 InstallInfo::Custom(InstallCustomInfo {
                     default_host_triple,
                     default_toolchain,
                     profile,
                     modify_path_variable,
-                },) => format!(
+                }) => format!(
                     "./cache/rustup-init.sh -y --default-host-triple='{}' \
                      --default-toolchain='{}' --profile='{}'{}",
                     default_host_triple,
@@ -246,32 +281,33 @@ impl crate::app::AppOper for Rustup {
                 .into(),
             };
             trace!("Shell command to execute: {shell}");
-            let mut command = Command::new("/usr/bin/sh",)
-                .stdin(Stdio::null(),)
-                .stdout(Stdio::piped(),)
-                .stderr(Stdio::piped(),)
-                .arg("-c",)
-                .arg(shell.as_ref(),)
+            let mut command = Command::new("/usr/bin/sh")
+                .stdin(Stdio::null())
+                .stdout(Stdio::piped())
+                .stderr(Stdio::piped())
+                .arg("-c")
+                .arg(shell.as_ref())
                 .spawn()
-                .map_err(Error::IOError,)?;
+                .map_err(Error::IOError)?;
             trace!("Command spawned successfully");
-            let (mut stdout, mut stderr,) =
-                (
-                    command.stdout.take().ok_or(Error::InnerError(
-                        "Command 'sh': stdout is not available".into(),
-                    ),)?,
-                    command.stderr.take().ok_or(Error::InnerError(
-                        "Command 'sh': stderr is not available".into(),
-                    ),)?,
-                );
-            let exit_status = command.wait().await.map_err(Error::IOError,)?;
+            let (mut stdout, mut stderr) = (
+                command
+                    .stdout
+                    .take()
+                    .ok_or(Error::InnerError("Command 'sh': stdout is not available".into()))?,
+                command
+                    .stderr
+                    .take()
+                    .ok_or(Error::InnerError("Command 'sh': stderr is not available".into()))?,
+            );
+            let exit_status = command.wait().await.map_err(Error::IOError)?;
             trace!("Command finished with exit status: {exit_status}");
-            let (mut stdout_buf, mut stderr_buf,) = (Vec::new(), Vec::new(),);
-            stdout.read_to_end(&mut stdout_buf,).await.map_err(Error::IOError,)?;
-            stderr.read_to_end(&mut stderr_buf,).await.map_err(Error::IOError,)?;
-            let (stdout_buf, stderr_buf,) = (
-                OsString::from_vec(stdout_buf,).to_string_lossy().into_owned(),
-                OsString::from_vec(stderr_buf,).to_string_lossy().into_owned(),
+            let (mut stdout_buf, mut stderr_buf) = (Vec::new(), Vec::new());
+            stdout.read_to_end(&mut stdout_buf).await.map_err(Error::IOError)?;
+            stderr.read_to_end(&mut stderr_buf).await.map_err(Error::IOError)?;
+            let (stdout_buf, stderr_buf) = (
+                OsString::from_vec(stdout_buf).to_string_lossy().into_owned(),
+                OsString::from_vec(stderr_buf).to_string_lossy().into_owned(),
             );
             info!("Command finished with stdout: \n{stdout_buf}");
             warn!("Command finished with stderr: \n{stderr_buf}");
@@ -279,40 +315,44 @@ impl crate::app::AppOper for Rustup {
                 Ok(Self {
                     // ~/.config
                     home_path: std::env::home_dir()
-                        .ok_or(Error::FailedToGetHomeDir,)?
-                        .join(".config",),
-                },)
+                        .ok_or(Error::FailedToGetHomeDir)?
+                        .join(".config"),
+                })
             } else {
                 Err(Error::Failed {
                     exit_status,
                     stdin: "".into(),
                     stdout: stdout_buf.into(),
                     stderr: stderr_buf.into(),
-                },)
+                })
             }
-        } else if cfg!(windows) {
+        }
+        #[cfg(windows)]
+        {
             todo!()
-        } else {
+        }
+        #[cfg(all(not(windows), not(unix)))]
+        {
             Err(Error::Unsupported(
                 format!("unsupported platform '{}'", std::env::consts::OS).into(),
-            ),)
+            ))
         }
     }
 
-    async fn reinstall(self, _info: Self::ReinstallInfo,) -> Result<Self,> { todo!() }
+    async fn reinstall(self, _info: Self::ReinstallInfo) -> Result<Self> { todo!() }
 
-    async fn remove(self, _info: Self::RemoveInfo,) -> Result<(),> { todo!() }
+    async fn remove(self, _info: Self::RemoveInfo) -> Result<()> { todo!() }
 
-    async fn update(self, _info: Self::UpdateInfo,) -> Result<Self,> { todo!() }
+    async fn update(self, _info: Self::UpdateInfo) -> Result<Self> { todo!() }
 }
 #[cfg(test)]
 mod tests {
     use super::*;
     use crate::app::AppOper;
     #[tokio::test]
-    async fn install_rustup() -> std::result::Result<(), Box<dyn std::error::Error,>,> {
+    async fn install_rustup() -> std::result::Result<(), Box<dyn std::error::Error>> {
         env_logger::init();
-        Rustup::install(InstallInfo::Default,).await?;
-        Ok::<_, Box<dyn std::error::Error,>,>((),)
+        Rustup::install(InstallInfo::Default).await?;
+        Ok::<_, Box<dyn std::error::Error>>(())
     }
 }
